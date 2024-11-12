4019a35e2e05d4bc3237a0aa2bd63aabf0f17cfc
tplocal <tristan.lesaux@yahoo.fr>
2024-11-12 15:30:40 +0100

    first commit

---
+++ ProjectDevIOS/ProjectDevIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDevIOSApp.swift
+//  ProjectDevIOS
+//
+//  Created by tplocal on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDevIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
